91800b0f127c14711ce7cffac8dd6631ad9bbfee
limou3434 <898738804@qq.com>
2025-04-24 22:12:51 +0800

    重构关于 hotkey 的部分

--- src/main/java/com/limou/intelligentinterview/MainApplication.java
+++ src/main/java/com/limou/intelligentinterview/MainApplication.java
@@ -7,24 +7,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- * 主类（项目启动入口）
+ * Spring Boot 启动类
  *
- * @author <a href="https://github.com/xiaogithubooo">limou3434</a>
- * @from <a href="https://datalearnhub.com">大数据工作室</a>
+ * @author <a href="https://github.com/limou3434">limou3434</a>
  */
 
 // @SpringBootApplication (exclude = {RedisAutoConfiguration.class}) // TODO: 如需开启 Redis，须移除 exclude 中的内容
 @SpringBootApplication
 @MapperScan("com.limou.intelligentinterview.mapper")
-@EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 // @ServletComponentScan // NOTE: 手动取消动态过滤
+@EnableScheduling // 开启定时任务
 @Slf4j
 public class MainApplication {
 
@@ -35,8 +32,8 @@ public class MainApplication {
         String baseUrl = "http://" + serverConfig.getAddress() + ":" + serverConfig.getPort() + serverConfig.getContextPath();
         log.info("Spring Boot running...");
         log.info("访问 {} 或 {} 即可得到在线文档, 访问 {} 即可得到文档配置", baseUrl + springdocConfig.getKnife4jUi(), baseUrl + springdocConfig.getSwaggerUi(), baseUrl + springdocConfig.getApiDocs());
-        log.debug("读取 Sa-token 配置查验是否正确: {}", String.valueOf(SaManager.getConfig()));
-        log.debug("读取 Sa-token 切面类查验是否被替换为自己的: {}", String.valueOf(SaManager.getStpInterface()));
+        log.debug("读取 Sa-token 配置查验是否正确: {}", SaManager.getConfig());
+        log.debug("读取 Sa-token 切面类查验是否被替换为自己的: {}", SaManager.getStpInterface());
     }
 
 }
